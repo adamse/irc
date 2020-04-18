35b7cbef339a3a852a83f4ff9b29d3547604ef45
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2020-04-18 20:27:39 +0100

    print dlerror

--- test.cpp
+++ test.cpp
@@ -9,14 +9,14 @@ int main(int argc, char **argv) {
   void* object = dlopen("./m_minimal.so", RTLD_LAZY);
 
   if (object == NULL) {
-    printf("failed to dlopen\n");
+    printf("failed to dlopen: %s\n", dlerror());
     return 1;
   }
 
   init_fun_type init_fun = (init_fun_type)dlsym(object, "inspircd_module_init");
 
   if (init_fun == NULL) {
-    printf("failed to dlsym\n");
+    printf("failed to dlsym: %s\n", dlerror());
     return 2;
   }
 
