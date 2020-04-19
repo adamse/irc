c3081b350adf3b8fedbec712a34cbe637c7ac33f
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2020-04-19 10:20:38 +0100

    create hie.yaml in shell.nix

--- shell.nix
+++ shell.nix
@@ -8,6 +8,11 @@ let
   ghcPkgs = pkgs.haskell.packages.${ghc};
   ghcWithPackages = ghcPkgs.ghcWithPackages (p: with p; [ base async text containers ]);
   ghcVersion = ghcPkgs.ghc.version;
+  hie = ''
+    cradle:
+      direct:
+        arguments: ["-package-db ${ghcWithPackages.out}/lib/ghc-${ghcVersion}/package.conf.d"]
+  '';
 in
 pkgs.mkShell {
   INC = "-I${inspircdSrc}/include";
@@ -17,4 +22,7 @@ pkgs.mkShell {
     ghcWithPackages
     pkgs.gcc
   ];
+  shellHook = ''
+    echo "${hie}" > hie.yaml
+  '';
 }
