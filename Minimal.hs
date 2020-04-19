ec61183edeb361b0b3306c6eed396e771c9de340
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2020-04-19 10:21:38 +0100

    add logging

--- Minimal.hs
+++ Minimal.hs
@@ -15,13 +15,14 @@ import Data.IORef (readIORef, IORef, newIORef)
 import Control.Concurrent.Chan (readChan, writeChan, Chan, newChan)
 import Control.Concurrent.Async (Async, async)
 import Foreign.C.Types (CChar)
-import Foreign (plusPtr, castPtr, Ptr)
+import Foreign (allocaBytes, FunPtr, plusPtr, castPtr, Ptr)
 import Foreign.Storable (Storable(..))
 import Data.Text (Text)
 import qualified Data.Text.Foreign as Text
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Control.Monad (forever)
+import qualified Data.Text as Text
 
 newtype TargetType = TargetType CInt
 
@@ -50,20 +51,46 @@ data State = State
   { known_channels :: IORef (Set Text)
   , ircd_message_queue :: Chan Message
   , ircd_message_worker :: Async ()
+  , ircd_logger :: Logger
   }
 
-foreign export ccall hs_module_init :: IO (StablePtr State)
+newtype LogLevel = LogLevel CInt
+
+pattern LOG_RAWIO :: LogLevel
+pattern LOG_RAWIO = LogLevel 5
+pattern LOG_DEBUG :: LogLevel
+pattern LOG_DEBUG = LogLevel 10
+pattern LOG_VERBOSE :: LogLevel
+pattern LOG_VERBOSE = LogLevel 20
+pattern LOG_DEFAULT :: LogLevel
+pattern LOG_DEFAULT = LogLevel 30
+pattern LOG_SPARSE :: LogLevel
+pattern LOG_SPARSE = LogLevel 40
+pattern LOG_NONE :: LogLevel
+pattern LOG_NONE = LogLevel 5
+
+type Logger = LogLevel -> Text -> IO ()
+type CLogger = LogLevel -> Ptr Text -> IO ()
+
+foreign import ccall "dynamic" mkLogger :: FunPtr CLogger -> CLogger
+
+foreign export ccall hs_module_init :: FunPtr CLogger -> IO (StablePtr State)
 foreign export ccall hs_module_cleanup :: StablePtr State -> IO ()
 foreign export ccall hs_module_version :: IO CString
 
-hs_module_init :: IO (StablePtr State)
-hs_module_init = do
+hs_module_init :: FunPtr CLogger -> IO (StablePtr State)
+hs_module_init logger' = do
+  let logger = mkLogger logger'
+  let ircd_logger lvl t = withCString t (logger lvl)
+
   known_channels <- newIORef ["chan"]
   ircd_message_queue <- newChan
 
   ircd_message_worker <- async $ forever do
     msg <- readChan ircd_message_queue
-    appendFile "m_minimal.log" (show msg <> "\n")
+    let msg' = "recieved: " <> show msg <> "\n"
+    ircd_logger LOG_DEFAULT (Text.pack msg')
+    appendFile "m_minimal.log" msg'
 
   newStablePtr State{..}
 
@@ -80,14 +107,23 @@ foreign export ccall hs_module_OnUserPostMessage :: StablePtr State -> Ptr Text 
 hs_module_OnUserPostMessage :: StablePtr State -> Ptr Text -> TargetType -> Ptr Text -> Ptr Text -> IO CInt
 hs_module_OnUserPostMessage state pnick targetType ptarget pmessage = do
   State{ircd_message_queue} <- deRefStablePtr state
-  target <- peekString ptarget
-  sender <- peekString pnick
-  message <- peekString pmessage
+  target <- peekCString ptarget
+  sender <- peekCString pnick
+  message <- peekCString pmessage
   writeChan ircd_message_queue Message{..}
   pure 1
 
-peekString :: Ptr Text -> IO Text
-peekString ptr = do
+peekCString :: Ptr Text -> IO Text
+peekCString ptr = do
   len :: CULong <- peek (castPtr ptr)
   data_ <- peek (ptr `plusPtr` sizeOf len)
   Text.peekCStringLen (data_, fromIntegral len)
+
+withCString :: Text -> (Ptr Text -> IO a) -> IO a
+withCString t act = Text.withCStringLen t \(data_, len') -> do
+  let len :: CULong = fromIntegral len'
+  let sz = sizeOf len + sizeOf data_
+  allocaBytes sz \ptr -> do
+    poke ptr len
+    poke (ptr `plusPtr` sizeOf len) data_
+    act (castPtr ptr)
--- m_minimal.cpp
+++ m_minimal.cpp
@@ -7,8 +7,13 @@ typedef struct {
   HsPtr data;
 } String;
 
-#define STR(std) {(std).length(), (HsPtr)(std).data()}
+#define HSSTR(std) {(std).length(), (HsPtr)(std).data()}
 #define PTR(str) (HsPtr)(&(str))
+#define CSTR(hs) std::string((const char*) (hs)->data, (size_t)(hs)->len)
+
+void HSLog(LogLevel lvl, String* hs) {
+  ServerInstance->Logs->Log("m_minimal", lvl, CSTR(hs));
+}
 
 class MinimalModule : public Module {
 
@@ -20,12 +25,12 @@ class MinimalModule : public Module {
     char* argv[] = {NULL};
     char** argp = argv;
     hs_init(&argc, &argp);
-    state = hs_module_init();
+    state = hs_module_init((HsFunPtr)&HSLog);
   }
 
   ~MinimalModule() {
     hs_module_cleanup(state);
-    hs_exit(); // the GHC rts cannot be reloaded currently, hence this is a bit fishy to do :)
+    hs_exit();
   }
 
   Version GetVersion() {
@@ -34,9 +39,9 @@ class MinimalModule : public Module {
   }
 
   void OnUserPostMessage(User* user, const MessageTarget& mt, const MessageDetails& details) override {
-    String nick = STR(user->nick);
-    String target = STR(mt.GetName());
-    String message = STR(details.text);
+    String nick = HSSTR(user->nick);
+    String target = HSSTR(mt.GetName());
+    String message = HSSTR(details.text);
 
     hs_module_OnUserPostMessage(state, PTR(nick), mt.type, PTR(target), PTR(message));
   }
